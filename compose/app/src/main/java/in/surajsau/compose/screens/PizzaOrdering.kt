a43e62a92d3b426d6b50bce89522e1359cb0bd3a
suraj <i@surajsau.in>
2020-12-10 16:34:19 +0530

    :lipstick: Added PizzaOrdering.kt

---
+++ compose/app/src/main/java/in/surajsau/compose/screens/PizzaOrdering.kt
@@ -0,0 +1,15 @@
+package `in`.surajsau.compose.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PizzaOrdering() {
+
+}
+
+@Preview
+@Composable
+fun previewPizzaOrdering() {
+    PizzaOrdering()
+}
